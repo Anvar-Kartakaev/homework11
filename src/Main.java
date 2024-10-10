82e8c3f807a9abff8f56a12835d60039014d57a0
john-wayne <anvar.kartakaev@gmail.com>
2024-10-10 12:26:56 +0500

    Reworking #2 HomeWork #11 for checking

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,33 @@ import java.time.LocalDate;
 
 public class Main {
 
+    //These are my old Tasks (updated)
+    public static void main(String[] args) {
+        System.out.println("Домашнее задание №11");
+
+        //Task 1
+        System.out.println("\nЗадание №1:");
+        int year = LocalDate.now().getYear(); // Проверял и с другими годами в том числе 100 и 400, работает
+        checkYear(year); //Method for Task 1
+
+
+        //Task 2
+        System.out.println("\nЗадание №2:");
+        int clientOS = 0;
+        int clientDeviceYear = LocalDate.now().getYear(); // Проверял и с другими годами, работает
+        checkPhone(clientOS, clientDeviceYear); //Method for Task 2
+
+
+        //Task 3
+        System.out.println("\nЗадание №3:");
+        int deliveryDistance = 95;
+        checkDistance(deliveryDistance); //Method for Task 3
+
+    }
+
     //Methods for Task 1
-    public static void leapYear(int year) {
-        if (year % 4 == 0 && year >= 1584) { // Согласно условиям задачи високосный год начинается с 1584
-            printLeapYear(year);
-        } else if (year % 100 == 0 && year >= 1584) {
-            printNotLeapYear(year);
-        } else if (year % 400 == 0 && year >= 1584) {
+    public static void checkYear(int year) {
+        if (year >= 1584 && (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)) {
             printLeapYear(year);
         } else {
             printNotLeapYear(year);
@@ -25,7 +45,7 @@ public class Main {
 
 
     //Methods for Task 2
-    public static void yourPhone(int clientOS, int clientDeviceYear) {
+    public static void checkPhone(int clientOS, int clientDeviceYear) {
         if (clientOS == 0 && clientDeviceYear >= 2015) {
             printVerdictNewIos();
         } else if (clientOS == 0) {
@@ -57,53 +77,33 @@ public class Main {
 
 
     //Methods for Task 3
-    public static void checkDistance(int deliveryDistance) {
+    public static int checkDistance(int deliveryDistance) {
+        int check = 0;
         int deliveryTime = 0;
         if (deliveryDistance <= 20) {
-            deliveryTime = deliveryTime + 1;
-            printDaysCount(deliveryDistance, deliveryTime);
+            deliveryTime++;
+            checkDaysCount(deliveryDistance, deliveryTime);
         } else if (deliveryDistance <= 60) {
-            deliveryTime = deliveryTime + 2;
-            printDaysCount(deliveryDistance, deliveryTime);
+            deliveryTime += 2;
+            checkDaysCount(deliveryDistance, deliveryTime);
         } else if (deliveryDistance <= 100) {
-            deliveryTime = deliveryTime + 3;
-            printDaysCount(deliveryDistance, deliveryTime);
+            deliveryTime += 3;
+            checkDaysCount(deliveryDistance, deliveryTime);
         } else {
-            printDaysCount(deliveryDistance, deliveryTime);
+            checkDaysCount(deliveryDistance, deliveryTime);
         }
+        return check;
     }
 
-    public static void printDaysCount(int deliveryDistance, int deliveryTime) {
+    public static int checkDaysCount(int deliveryDistance, int deliveryTime) {
+        int checkCount = 0;
         if (deliveryDistance <= 100) {
             System.out.println("Потребуется дней " + deliveryTime);
         } else {
             System.out.println("Свыше 100 км доставки нет");
         }
+        return checkCount;
     }
 
 
-    //These are my old Tasks (updated)
-    public static void main(String[] args) {
-        System.out.println("Домашнее задание №11");
-
-        //Task 1
-        System.out.println("\nЗадание №1:");
-        int year = LocalDate.now().getYear(); // Проверял и с другими годами в том числе 100 и 400, работает
-        leapYear(year); //Method for Task 1
-
-
-        //Task 2
-        System.out.println("\nЗадание №2:");
-        int clientOS = 0;
-        int clientDeviceYear = LocalDate.now().getYear(); // Проверял и с другими годами, работает
-        yourPhone(clientOS, clientDeviceYear); //Method for Task 2
-
-
-        //Task 3
-        System.out.println("\nЗадание №3:");
-        int deliveryDistance = 95;
-        checkDistance(deliveryDistance); //Method for Task 3
-
-
-    }
 }
